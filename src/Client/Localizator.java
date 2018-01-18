5ec22dc356b8970158bc694e50883b0f95669e02
GroovyChuk <alasdair333@hotmail.com>
2018-01-18 21:16:00 +0100

    filterParticles now performs the resampling of Particles

--- src/Client/Localizator.java
+++ src/Client/Localizator.java
@@ -3,6 +3,7 @@ package src.Client;
 /**
  * Created by Kalaman on 17.01.18.
  */
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -57,14 +58,40 @@ public class Localizator {
         }
     }
 
+    /**
+     * Performs the resampling of Particles according to the weights
+     * @param sensorRange
+     */
     public void filterParticles(float sensorRange){
         ArrayList<Particle> temp_particleList = new ArrayList<>();
+        int maxValue = 0;
 
         for (Particle particle : particleList) {
             particle.evaluateParticle(roomMap,sensorRange);
-            if (particle.getWeight() != 0)
-                temp_particleList.add(particle);
+            maxValue += particle.getWeight();
         }
+
+        int index = (int) Math.random() * particleList.size();
+        float beta = 0.0f;
+
+        for (int i = 0; i < particleList.size(); i++) {
+            beta += (Math.random() * 2 * maxValue);
+            while (beta > particleList.get(index).getWeight()){
+                beta -= particleList.get(index).getWeight();
+                index = (index + 1) % particleList.size();
+            }
+            temp_particleList.add(generateNewParticle(particleList.get(index)));
+        }
+
         particleList = temp_particleList;
+    }
+
+    public Particle generateNewParticle(Particle oldParticle){
+        int degree = (int) (Math.random() * 360);
+        int endX = (int) (oldParticle.getPositionX() + (Math.cos(Math.toRadians(degree)) * 10));
+        int endY = (int) (oldParticle.getPositionY() + (Math.sin(Math.toRadians(degree)) * 10));
+        Particle newParticle = new Particle(endX, endY, oldParticle.getDegree(),0);
+        float distance = (float) Math.sqrt(Math.pow(oldParticle.getPositionX()-endX,2) + Math.pow(oldParticle.getPositionY()-endY,2));;
+        return newParticle;
     }
 }
