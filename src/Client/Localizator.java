8ede153349b3a74c03fffdc657ba3f1dbd53f545
Kalaman <oemer.kalaman@hotmail.de>
2018-02-04 22:30:56 +0100

    filterParticle() uses normalize() for all particles now

--- src/Client/Localizator.java
+++ src/Client/Localizator.java
@@ -33,28 +33,17 @@ public class Localizator {
 
         ArrayList<Line> lines = roomMap.getRoomLines();
         for (int i=0;i<initParticleAmount;++i) {
-            boolean found = false;
 
-            int randomX = 0;
-            int randomY = 0;
+            int randomX = -1;
+            int randomY = -1;
 
-            while (!found) {
+            while (!isValidPosition(randomX,randomY)) {
                 randomX = (int) (Math.random() * roomMap.getSvgDiagram().getWidth());
                 randomY = (int) (Math.random() * roomMap.getSvgDiagram().getHeight());
-
-                for (int j = 0; j < lines.size(); ++j) {
-                    Line currentLine = lines.get(j);
-                    if (randomX >= currentLine.getX1() && randomX < currentLine.getX2() &&
-                            randomY >= currentLine.getY2()) {
-                        found = true;
-                        break;
-                    }
-                }
             }
 
             float randomDeg = (float) (Math.random() * 360) ;
-
-            particleList.add(new Particle(randomX,randomY,randomDeg,0));
+            particleList.add(new Particle(randomX,randomY,randomDeg,1f / JConstants.PARTICLE_AMOUNT));
         }
     }
 
@@ -64,63 +53,75 @@ public class Localizator {
      */
     public void filterParticles(float sensorRange){
         ArrayList<Particle> temp_particleList = new ArrayList<>();
-        int maxValue = 0;
+        double sumValue = 0;
 
         for (Particle particle : particleList) {
             particle.evaluateParticle(roomMap,sensorRange);
-            maxValue += particle.getWeight();
+            sumValue += particle.getWeight();
         }
 
-        int index = (int) Math.random() * particleList.size();
+        for (Particle particle: particleList) {
+            particle.normalize(sumValue);
+        }
+
+        int index = (int) (Math.random() * particleList.size());
         float beta = 0.0f;
 
         for (int i = 0; i < particleList.size(); i++) {
-            beta += (Math.random() * 2 * maxValue);
+            beta += (Math.random() * 2 * sumValue);
             while (beta > particleList.get(index).getWeight()){
                 beta -= particleList.get(index).getWeight();
                 index = (index + 1) % particleList.size();
             }
-            if ((i % 200) == 0){
-                temp_particleList.add(generateNewParticle(particleList.get(index),true));
-            } else {
-                temp_particleList.add(generateNewParticle(particleList.get(index),false));
-            }
 
+            temp_particleList.add(generateNewParticle(particleList.get(index),false));
+        }
+
+        /**
+         * For debug purposes
+         * Finds the particle with the highest weight
+         */
+        int hindex = 0;
+        double hweight = 0;
+        for (int i=0;i<temp_particleList.size();++i) {
+            if (temp_particleList.get(i).getWeight() > hweight)
+            {
+                hweight = temp_particleList.get(i).getWeight();
+                hindex = i;
+            }
         }
 
         particleList = temp_particleList;
     }
 
     public Particle generateNewParticle(Particle oldParticle, boolean random){
+        int newX = -1, newY = -1;
 
-        boolean found = false;
-        int newX = 0, newY = 0;
-        while (!found) {
+        while (!isValidPosition(newX,newY)) {
             if (random){
                 newX = (int) (Math.random() * roomMap.getSvgDiagram().getWidth());
                 newY = (int) (Math.random() * roomMap.getSvgDiagram().getHeight());
             } else {
-                int degree = (int) (Math.random() * 360);
-                newX = (int) (oldParticle.getPositionX() + (Math.cos(Math.toRadians(degree)) * 10) + 1);
-                newY = (int) (oldParticle.getPositionY() + (Math.sin(Math.toRadians(degree)) * 10));
-            }
-
-            ArrayList<Line> lines = roomMap.getRoomLines();
-
-
-            //Check if Particle has valid location
-            for (int j = 0; j < lines.size(); ++j) {
-                Line currentLine = lines.get(j);
-                if (newX >= currentLine.getX1() && newX < currentLine.getX2() &&
-                        newY >= currentLine.getY2()) {
-                    found = true;
-                    break;
-                }
+                newX = ThreadLocalRandom.current().nextInt(oldParticle.getPositionX() - 10, oldParticle.getPositionX() + 10 + 1) ;
+                newY = ThreadLocalRandom.current().nextInt(oldParticle.getPositionY() - 10, oldParticle.getPositionY() + 10 + 1) ;
             }
         }
+        int newDegree =  ThreadLocalRandom.current().nextInt(oldParticle.getDegree() - 25, oldParticle.getDegree() + 25 + 1) ;
 
-        Particle newParticle = new Particle(newX, newY, oldParticle.getDegree(),0);
-        float distance = (float) Math.sqrt(Math.pow(oldParticle.getPositionX()-newX,2) + Math.pow(oldParticle.getPositionY()-newY,2));;
+        Particle newParticle = new Particle(newX, newY, newDegree,oldParticle.getWeight());
         return newParticle;
+    }
+
+    public boolean isValidPosition (int x,int y) {
+        ArrayList<Line> lines = roomMap.getRoomLines();
+
+        for (int j = 0; j < lines.size(); ++j) {
+            Line currentLine = lines.get(j);
+            if (x >= currentLine.getX1() && x < currentLine.getX2() &&
+                    y >= currentLine.getY2()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
