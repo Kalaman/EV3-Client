68b4f44b20c25c6b9733ba3a43884bbab649683a
Kalaman <oemer.kalaman@hotmail.de>
2018-02-04 22:17:46 +0100

    Added Virtual Robot

--- src/Client/JConstants.java
+++ src/Client/JConstants.java
@@ -11,6 +11,7 @@ public class JConstants {
     public static final int PANEL_ROBOT_SIZE_X = 700;
     public static final int PANEL_CONSOLE_SIZE_X = WINDOW_SIZE_X - PANEL_ROBOT_SIZE_X;
 
+    public static final int PARTICLE_AMOUNT = 1000;
     public static final int PARTICLE_WIDTH = 6;
     public static final int PARTICLE_HEIGHT = 6;
     public static final int PARTICLE_DEGREE_MULTIPLICATOR = 5;
--- src/Client/JRobotPanel.java
+++ src/Client/JRobotPanel.java
@@ -6,6 +6,8 @@ import com.kitfox.svg.SVGUniverse;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
 import java.net.URI;
 import java.util.ArrayList;
@@ -15,6 +17,9 @@ public class JRobotPanel extends JPanel{
     Graphics2D graphics2D;
     public static Localizator localizator;
     RoomMap roomMap;
+
+    int virtualRobotX = 15;
+    int virtualRobotStep = 10;
 
     public JRobotPanel() {
         setPreferredSize(new Dimension(JConstants.PANEL_ROBOT_SIZE_X,JConstants.WINDOW_SIZE_Y));
@@ -34,6 +39,7 @@ public class JRobotPanel extends JPanel{
             public void onUltrasonicDistanceReceived(float distanceInCM) {
                 JConsolePanel.writeToConsole("New distance to wall: " + distanceInCM);
                 JRobotPanel.localizator.filterParticles(distanceInCM);
+                virtualRobotX += virtualRobotStep;
                 repaint();
             }
 
@@ -64,6 +70,7 @@ public class JRobotPanel extends JPanel{
         catch (Exception e){ e.printStackTrace();}
 
         drawParticles(Color.RED,graphics2D);
+        drawParticle(new Particle(virtualRobotX,65,0,0),Color.BLUE,graphics2D);
     }
 
 
@@ -88,27 +95,4 @@ public class JRobotPanel extends JPanel{
         graphics2D.setColor(oldColor);
     }
 
-    public void testParticle(Graphics2D graphics2D){
-        Particle particle = new Particle(260,70,193,0);
-        Point p=particle.findIntersection(245,0,245,50);
-        drawParticle(particle,Color.RED, (Graphics2D) graphics2D);
-        particle.drawParticleLine(Color.RED, (Graphics2D) graphics2D);
-
-        System.out.println(particle.getDistanceToWall(roomMap.getRoomLines()));
-        graphics2D.drawOval( (int) particle.intersection.getX() - 2, (int) particle.intersection.getY() -2, 4,4);
-
-//        particle.evaluateParticle(roomMap,19.0f);
-//        System.out.println(particle.getWeight());
-
-//        localizator.filterParticles(20,1);
-//        try {
-//            TimeUnit.SECONDS.sleep(3);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        repaint();
-
-//        Particle p = new Particle(50,50,270,1);
-//        localizator.generateNewParticle(p);
-    }
 }
